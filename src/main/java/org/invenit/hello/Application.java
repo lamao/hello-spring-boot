fa9741be072aeeeb5198c59bbff983059c82ea30
vmischeryakov <vmischeryakov@gmail.com>
2017-06-29 09:14:17 +0300

    Basic authentication (http://www.devglan.com/spring-security/spring-boot-security-rest-basic-authentication)

--- src/main/java/org/invenit/hello/Application.java
+++ src/main/java/org/invenit/hello/Application.java
@@ -3,9 +3,10 @@ package org.invenit.hello;
 import org.invenit.hello.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(new Class<?>[] {Application.class, JpaConfig.class}, args);
