b14b88e7c45428c2086d57e8d8712931d4b512fb
vmischeryakov <vmischeryakov@gmail.com>
2017-07-04 12:35:11 +0300

    Authentication. From DB

--- src/main/java/org/invenit/hello/config/SpringSecurityConfig.java
+++ src/main/java/org/invenit/hello/config/SpringSecurityConfig.java
@@ -1,10 +1,21 @@
 package org.invenit.hello.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 /**
@@ -16,14 +27,37 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationEntryPoint authenticationEntryPoint;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic()
-                        .authenticationEntryPoint(authenticationEntryPoint);
+        http.csrf().disable()
+        /*  */.authorizeRequests().anyRequest().authenticated()
+        /*  */.and().httpBasic().authenticationEntryPoint(authenticationEntryPoint);
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("1").roles("USER");
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setDataSource(dataSource);
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        auth.userDetailsService(userDetailsManager).passwordEncoder(encoder);
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+        configureDefaultUser(userDetailsManager, encoder);
+    }
+
+    private void configureDefaultUser(
+                    JdbcUserDetailsManager userDetailsService,
+                    PasswordEncoder encoder) {
+        if (!userDetailsService.userExists("user")) {
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("USER"));
+            User userDetails = new User("user", encoder.encode("password"), authorities);
+
+            userDetailsService.createUser(userDetails);
+        }
     }
 }
