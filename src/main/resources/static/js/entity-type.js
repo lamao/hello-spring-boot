b35fd2b9e684edd1c93d43ed938e0c7d32aee2a5
vmischeryakov <vmischeryakov@gmail.com>
2017-08-03 09:49:36 +0300

    Entity & entity type lists

--- src/main/resources/static/js/entity-type.js
+++ src/main/resources/static/js/entity-type.js
@@ -1,4 +1,7 @@
 
 app.controller('entityTypeListController', ['$scope', '$http', '$q', 'constants', function($scope, $http, $q, constants) {
 
+  $http.get("/api/entity-type").then(function(response) {
+    $scope.items = response.data.content;
+  });
 }]);
--- src/main/resources/static/js/entity.js
+++ src/main/resources/static/js/entity.js
@@ -1,3 +1,5 @@
 app.controller('entityListController', ['$scope', '$http', '$q', 'constants', function($scope, $http, $q, constants) {
-
+  $http.get("/api/entity").then(function(response){
+    $scope.items = response.data.content;
+  })
 }]);
