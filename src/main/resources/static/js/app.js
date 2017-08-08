33810f7edc66e7e5dc6746e516bbc0d204d2c14c
vmischeryakov <vmischeryakov@gmail.com>
2017-08-08 09:54:07 +0300

    Add entity page

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -8,6 +8,10 @@ app.config(function($locationProvider, $routeProvider) {
       templateUrl: '/views/entity-list.html',
       controller: 'entityListController'
     })
+    .when('/entities/add', {
+      templateUrl: '/views/entity-edit.html',
+      controller: 'entityEditController'
+    })
     .when('/entity-types', {
       templateUrl: '/views/entity-type-list.html',
       controller: 'entityTypeListController'
--- src/main/resources/static/js/entity.js
+++ src/main/resources/static/js/entity.js
@@ -3,3 +3,22 @@ app.controller('entityListController', ['$scope', '$http', '$q', 'constants', fu
     $scope.items = response.data.content;
   })
 }]);
+
+app.controller('entityEditController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+
+  $scope.item = {};
+
+  $http.get("/api/entity-type")
+    .then(function(response) {
+      $scope.entityTypes = response.data.content;
+    });
+
+
+  $scope.onSave = function() {
+    $http.post("/api/entity", $scope.item)
+      .then(function(response){
+        $location.path("/entities");
+      });
+  };
+
+}]);
